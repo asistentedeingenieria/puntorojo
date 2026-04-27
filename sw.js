f6755ba27ac69953fc45889ef001030d9b7a11a4
Antonio Caravantes <asistentedeingenieria@puntorojosa.com>
2026-04-27 09:51:24 -0600

    ACT20

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Para forzar actualización: subir el número de CACHE_VERSION.
    ════════════════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'v32-parser-excel-oficial-inteligente';
+const CACHE_VERSION = 'v34-fix-torre4-y-vista-pdf';
 const CACHE_NAME = 'puntorojo-' + CACHE_VERSION;
 
 // Archivos básicos que se cachean al instalar
