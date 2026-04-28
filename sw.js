841162d09c9c4056fbe02d1c9fcb67850541a440
Antonio Caravantes <asistentedeingenieria@puntorojosa.com>
2026-04-27 18:25:02 -0600

    Quitar mensaje acceso facial

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Para forzar actualización: subir el número de CACHE_VERSION.
    ════════════════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'v45-boton-limpiar-vacias';
+const CACHE_VERSION = 'v46-quitar-mensaje-acceso-facial';
 const CACHE_NAME = 'puntorojo-' + CACHE_VERSION;
 
 // Archivos básicos que se cachean al instalar
