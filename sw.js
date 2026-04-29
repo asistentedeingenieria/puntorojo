1192b662ea691d4ef3288d72c48729760c61e74b
Antonio Caravantes <asistentedeingenieria@puntorojosa.com>
2026-04-28 23:01:17 -0600

    ACT66

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Para forzar actualización: subir el número de CACHE_VERSION.
    ════════════════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'v91-priorizar-con-iva-en-excel';
+const CACHE_VERSION = 'v92-parser-robusto-con-iva-multi-formato';
 const CACHE_NAME = 'puntorojo-' + CACHE_VERSION;
 
 // Archivos básicos que se cachean al instalar
